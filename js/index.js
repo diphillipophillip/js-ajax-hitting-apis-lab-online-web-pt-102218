bb81763096bf8555f81a18772f7b848aa17d6010
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-06-14 14:25:27 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // your code here
+function getRepositories() {
+  const username = document.getElementById("username").value
+  const req = new XMLHttpRequest();
+  req.open('GET', `https://api.github.com/users/${username}/repos`)
+  req.send()
+}
+
+function displayRepositories() {
+  
+}
